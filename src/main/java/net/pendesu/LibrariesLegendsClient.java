a52c29d8b3fad6dc81e3b613baa4c88b302beb21
Jonathan Kat <15jonathankat@gmail.com>
2026-07-09 08:47:20 -0400

    Initial Commit to Git

---
+++ src/main/java/net/pendesu/LibrariesLegendsClient.java
@@ -0,0 +1,10 @@
+package net.pendesu;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LibrariesLegendsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/pendesu/LibrariesLegendsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pendesu;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LibrariesLegendsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
