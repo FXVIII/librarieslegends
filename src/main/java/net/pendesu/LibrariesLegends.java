223f79f9c420b38f7a638a0736aad6b93e95e93d
Jonathan Kat <15jonathankat@gmail.com>
2026-07-09 10:26:18 -0400

    book_bundle textures

--- src/main/java/net/pendesu/LibrariesLegends.java
+++ src/main/java/net/pendesu/LibrariesLegends.java
@@ -2,8 +2,7 @@ package net.pendesu;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.minecraft.util.Identifier;
-
+import net.pendesu.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +16,7 @@ public class LibrariesLegends implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+			ModItems.registerModItems();
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
---
+++ src/main/java/net/pendesu/item/ModItems.java
@@ -0,0 +1,30 @@
+package net.pendesu.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.pendesu.LibrariesLegends;
+
+public class ModItems {
+    public static final Item BOOK_BUNDLE = registerItem("book_bundle", new Item.Settings());
+
+    public static Item registerItem(String name, Item.Settings settings) {
+        Identifier id = Identifier.of(LibrariesLegends.MOD_ID + ":" + name);
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, id);
+        Item item = new Item(settings.registryKey(key));
+        return Registry.register(Registries.ITEM, key, item);
+    }
+
+    public static void registerModItems() {
+        LibrariesLegends.LOGGER.info("Registering ModItems for" + LibrariesLegends.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(BOOK_BUNDLE);
+        });
+    }
+}
